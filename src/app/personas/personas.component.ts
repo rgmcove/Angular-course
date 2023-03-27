aca07ab8ec18157f6f7b2fa3b2d4639536b6ed16
rgmcove <ralberto115@gmail.com>
2023-03-27 17:34:24 -0500

    Event Binding

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -8,5 +8,10 @@ import { Component } from "@angular/core";
 export class PersonasComponent{
 
     deshabilitar = false;
+    mensaje = 'No se ha agregado ninguna persona';
+
+    agregarPersona() {
+        this.mensaje = 'Persona agregada';
+    }
 
 }
