1f314982ab0a5d3c02d89c293707e40e03de253c
rago.mos <ralberto115@gmail.com>
2025-03-21 12:42:32 -0500

    refactor: modificacion componente app

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-angular';
+  title = 'Hello World from Angular';
 }
