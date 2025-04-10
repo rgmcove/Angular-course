3df1fc32aca36dccfada54b5d81658a1307418c7
rago.mos <ralberto115@gmail.com>
2025-04-10 11:50:04 -0500

    feat: uso de varios componentes en angular

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -3,14 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 import { ComponenteInlineComponent } from "./componente-inline/componente-inline.component";
 import { InterpolacionComponent } from "./interpolacion/interpolacion.component";
+import { PadreComponent } from "./padre/padre.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteInlineComponent, InterpolacionComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteInlineComponent, InterpolacionComponent, PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hello World from Angular';
+  title = 'Universidad Angular';
 }
